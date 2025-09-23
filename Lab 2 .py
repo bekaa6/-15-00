33e05429ef152ade3289c43cb09e7cf114c03994
bekaa6 <bekarys222dhhd@gmail.com>
2025-09-23 16:08:56 +0500

    Add files via upload

---
+++ Lab 2 .py
@@ -0,0 +1,35 @@
+a = 10
+b = 4
+result = a - b
+print(result)
+
+a = 7
+b = 5
+result = a + b
+print(result)
+
+a = 6
+b = 3
+result = a * b
+print(result)
+
+a = 2
+b = 4
+result = a ** b
+print(result)
+
+a = 9
+b = 4
+result = a / b
+print(result)
+
+a = 9
+b = 4
+result = a // b
+print(result)
+
+a = 9
+b = 4
+result = a % b
+print(result)
+
