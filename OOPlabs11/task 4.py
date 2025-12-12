2cb5ef8adde3b05768e88cf07b638a992b43b44d
bekaa6 <bekarys222dhhd@gmail.com>
2025-12-12 22:13:30 +0500

    Add files via upload

---
+++ OOPlabs11/task 4.py
@@ -0,0 +1,31 @@
+class BaseService:
+    def __init__(self):
+        print("Initializing BaseService.")
+
+
+class Logging(BaseService):
+    def __init__(self):
+        super().__init__()
+        print("Adding Logging functionality.")
+
+
+class Caching(BaseService):
+    def __init__(self):
+        super().__init__()
+        print("Adding Caching functionality.")
+
+
+class DataProcessor(Logging, Caching):
+    def __init__(self):
+        # super() вызывает следующий класс в MRO,
+        # который будет Caching, а Caching вызовет BaseService.
+        super().__init__()
+        print("Initializing DataProcessor.")
+
+
+# Вызов
+processor = DataProcessor()
+
+# Вывод MRO (для демонстрации порядка):
+print("\nMethod Resolution Order (MRO):")
+print(DataProcessor.__mro__)OOPlabs11
